d3e2afcf8e89d4a0fc33c490713bb818d39eead0
Ben Campbell <ben@scumways.com>
2017-05-09 23:50:36 +1200

    fiddling with config page and styling

--- facelog/conf.js
+++ facelog/conf.js
@@ -1,16 +1,39 @@
 (function() {
-var form = document.getElementById("form-register");
+var form1 = document.getElementById("form1");
+var form2 = document.getElementById("form2");
 
-form.addEventListener("submit", function(ev) {
+
+
+function regUser(details) {
+    var req = new XMLHttpRequest();
+    req.open('POST', 'http://localhost:8080/register', true);
+    req.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    req.addEventListener("load", function(ev) {
+        console.log("sent: ", ev);
+    });
+    req.addEventListener("error", function(ev) {
+        console.log("poop: ", ev);
+    });
+    console.log("sending registration details",details);
+    req.send( JSON.stringify(details) );
+}
+
+
+
+form1.addEventListener("submit", function(ev) {
+    ev.preventDefault();
+    regUser({ "ipsosmori": form1.ipsosmori.value });
+}, false);
+
+form2.addEventListener("submit", function(ev) {
     ev.preventDefault();
 
-    var foo = {
-        "postcode": form.postcode.value,
-        "age": form.age.value,
-        "gender": form.gender.value,
-    };
-    console.log(foo);
-
+    regUser({
+        "country": form2.country.value,
+        "postcode": form2.postcode.value,
+        "age": form2.age.value,
+        "gender": form2.gender.value,
+    });
 }, false);
 
 })();
